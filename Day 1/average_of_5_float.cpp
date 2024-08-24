a2463aff7244fc3544b3a3042bf1fc474fdf6c0c
Kartik2048 <142119629+Kartik2048@users.noreply.github.com>
2024-08-24 19:45:03 +0530

    day1

---
+++ Day 1/average_of_5_float.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    float a, b, c, d, e;
+    cout << "Enter grade 1: ";
+    cin >> a;
+    cout << "Enter grade 2: ";
+    cin >> b;
+    cout << "Enter grade 3: ";
+    cin >> c;
+    cout << "Enter grade 4: ";
+    cin >> d;
+    cout << "Enter grade 5: ";
+    cin >> e;
+    float av;
+    av=(a+b+c+d+e)/5;
+    cout << "The average grade is: " << av << endl;
+    }
---
+++ Day 1/square.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    cout << "Enter a number: ";
+    int a;
+    cin >> a;
+    cout << "The square of " << a << " is: " << a*a;
+    }
