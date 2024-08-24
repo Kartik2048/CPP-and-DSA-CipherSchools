ab5e2d63ee2729b31eecc7361dce6611918b4c78
Kartik2048 <142119629+Kartik2048@users.noreply.github.com>
2024-08-24 19:36:52 +0530

    version1

---
+++ Day 1/string-concatenation.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string a;
+    string b;
+    cout << "Enter the first string: ";
+    cin >> a;
+    cout << "Enter the second string: ";
+    cin >> b;
+    cout << "Concatenated string: " << a+b;
+    }
